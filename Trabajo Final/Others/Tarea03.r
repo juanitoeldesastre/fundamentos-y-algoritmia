f9435357a104f0b1f0e03fa901c9a80b3addaae9
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-11-06 22:29:41 -0500

    Eliminar archivos.py innecesarios y agregar archivo .R en el cual hay un ejemplo simple de media y mediana

---
+++ Trabajo Final/Others/Tarea03.r
@@ -0,0 +1,9 @@
+# Ejemplo de cálculo de la media
+data <- c(2, 4, 6, 8, 10)
+media <- mean(data)
+print(paste("Media:", media))
+
+# Ejemplo de cálculo de la mediana
+data <- c(2, 4, 6, 8, 10)
+mediana <- median(data)
+print(paste("Mediana:", mediana))
