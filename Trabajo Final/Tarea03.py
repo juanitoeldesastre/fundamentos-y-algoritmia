3d0f2c67896152d43c43eadceb5516ac1a26b1f5
juanitoeldesastre <juanitoeldevastador@gmail.com>
2024-11-06 22:23:47 -0500

    Add .ipynb file named Tarea03 which contains examples of what mean and median are, along with explanations

---
+++ Trabajo Final/Tarea03.py
@@ -0,0 +1,11 @@
+import statistics
+
+# Ejemplo de cálculo de la mediana
+data = [2, 4, 6, 8, 10]
+mediana = statistics.median(data)
+print("Mediana:", mediana)
+
+# Ejemplo de cálculo de la media
+data = [2, 4, 6, 8, 10]
+media = sum(data) / len(data)
+print("Media:", media)
